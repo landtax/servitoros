8515eac2e7b37f31e1dc02a58285d72636f3fac3
Santi <santiago.bel@webfordesigners.com>
2013-06-11 10:28:03 +0200

    detect uris in outputs and show accordingly

--- app/helpers/executions_helper.rb
+++ app/helpers/executions_helper.rb
@@ -7,4 +7,13 @@ module ExecutionsHelper
        end
   end
 
+  def uri?(string)
+    uri = URI.parse(string)
+    %w( http https ).include?(uri.scheme)
+  rescue URI::BadURIError
+    false
+  rescue URI::InvalidURIError
+    false
+  end
+
 end
